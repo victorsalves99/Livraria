6385bba24dfe37ae247271fc9f19908431681466
victorsalves99 <victorsalves99@outlook.com>
2023-01-22 18:16:07 -0300

    tentativa de recuperação falha

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ console.log(input.placeholder)
 input.addEventListener("keydown",() =>{
     const lupa = document.querySelector(".lupa")
     console.log(input.value)
-    if(input.value){
+    if(input.value !== ""){
         lupa.style.visibility = "hidden"
         return
     }
         lupa.style.visibility = "visible"
+    
     
 
   
